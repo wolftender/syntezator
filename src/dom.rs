a55600e51757dfc2721f2f5f6eeda0581adab710
wolftender <etheryt@protonmail.com>
2025-10-26 13:42:59 +0100

    add midi loader element

--- src/dom.rs
+++ src/dom.rs
@@ -1,71 +1,81 @@
 //! Handles to DOM elements in the HTML, and helper functions for interacting with JS.
-use wasm_bindgen::prelude::*;
-use wasm_bindgen_futures::spawn_local;
-use web_sys::{AudioBuffer, Document, js_sys};
+use std::{cell::RefCell, rc::Rc};
 
+use wasm_bindgen::prelude::*;
+use web_sys::{Document, FileReader, js_sys::Uint8Array};
+
+use crate::midi;
+
+#[allow(dead_code)]
 pub struct MidiInput {
     element: web_sys::HtmlInputElement,
+    midi_cb: Rc<RefCell<dyn FnMut(midi::MIDIFileData)>>,
+    error_cb: Rc<RefCell<dyn FnMut(midi::MIDIFileError)>>,
 }
 
 impl MidiInput {
-    pub fn new(document: &Document) -> Self {
+    pub fn new<F: FnMut(midi::MIDIFileData) + 'static, E: FnMut(midi::MIDIFileError) + 'static>(
+        document: &Document,
+        midi_cb: F,
+        error_cb: E,
+    ) -> Self {
         let element = document
             .get_element_by_id("midi")
             .expect("MIDI input element not found")
             .dyn_into::<web_sys::HtmlInputElement>()
             .expect("failed to cast midi input to HtmlInputElement");
 
-        Self { element }
-    }
+        let midi_cb = Rc::new(RefCell::new(midi_cb));
+        let error_cb = Rc::new(RefCell::new(error_cb));
+        let midi_cb_c = midi_cb.clone();
+        let error_cb_c = error_cb.clone();
 
-    pub fn listen_to_source_change(&self, callback: &dyn Fn(Vec<u8>)) {
-        let this = self.element.clone();
+        let on_change_closure = Closure::wrap(Box::new(move |event: web_sys::Event| {
+            let input: web_sys::HtmlInputElement = event
+                .target()
+                .unwrap()
+                .dyn_into()
+                .expect("cannot get correct target for change");
 
-        let closure = Closure::<dyn Fn(_)>::new(move |e: web_sys::InputEvent| {
-            let Some(first) = this.files().and_then(|f| f.item(0)) else {
-                // no file selected
-                return;
-            };
+            if let Some(file) = input.files().and_then(|f| f.item(0)) {
+                let reader = FileReader::new().expect("failed to create file reader");
+                let midi_cb_c = midi_cb_c.clone();
+                let error_cb_c = error_cb_c.clone();
 
-            spawn_local(async move {
-                let bytes = wasm_bindgen_futures::JsFuture::from(first.bytes())
-                    .await
-                    .expect("could not get bytes");
-                let bytes = js_sys::Uint8Array::new(&bytes).to_vec();
+                let on_load_closure = Closure::wrap(Box::new(move |event: web_sys::Event| {
+                    let reader: web_sys::FileReader = event
+                        .target()
+                        .unwrap()
+                        .dyn_into()
+                        .expect("cannot get correct target for load");
 
-                callback(bytes);
-                log::info!("Array buffer loaded: {bytes:?}");
-            });
-        });
+                    let array_buffer = reader.result().expect("failed to get result");
+                    let buffer = Uint8Array::new(&array_buffer).to_vec();
 
-        self.element
-            .add_event_listener_with_callback("change", closure.as_ref().unchecked_ref())
-            .expect("failed to add midi file listener");
+                    match midi::MIDIFileData::try_from(&buffer[..]) {
+                        Ok(data) => (midi_cb_c.borrow_mut())(data),
+                        Err(error) => (error_cb_c.borrow_mut())(error),
+                    }
+                }) as Box<dyn FnMut(_)>);
 
-        closure.forget();
-    }
-}
+                reader.set_onload(Some(on_load_closure.as_ref().unchecked_ref()));
+                reader
+                    .read_as_array_buffer(&file)
+                    .expect("cannot read as array buffer");
 
-pub struct AudioControl {
-    element: web_sys::HtmlAudioElement,
-    source: web_sys::HtmlSourceElement,
-}
+                on_load_closure.forget();
+            }
+        }) as Box<dyn FnMut(_)>);
 
-impl AudioControl {
-    pub fn new(document: &Document) -> Self {
-        let element = document
-            .get_element_by_id("audio")
-            .expect("audio control element not found")
-            .dyn_into::<web_sys::HtmlAudioElement>()
-            .expect("failed to cast audio control to HtmlAudioElement");
+        element
+            .add_event_listener_with_callback("change", on_change_closure.as_ref().unchecked_ref())
+            .expect("failed to set change event handler");
+        on_change_closure.forget();
 
-        let source = element
-            .query_selector("source")
-            .expect("bad query")
-            .expect("failed to find audio source")
-            .dyn_into::<web_sys::HtmlSourceElement>()
-            .expect("failed to cast audio source to HtmlSourceElement");
-
-        Self { element, source }
+        Self {
+            element,
+            midi_cb,
+            error_cb,
+        }
     }
 }
