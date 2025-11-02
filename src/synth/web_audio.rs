092fb7d20b86fc6c1d547af5d095af0e432583f8
shilangyu <xmarcinmarcin@gmail.com>
2025-11-02 20:43:06 +0100

    Use on/off velocity

--- src/synth/web_audio.rs
+++ src/synth/web_audio.rs
@@ -49,47 +49,44 @@ impl MidiSynth {
                 time += tick_duration * event.delta_time();
 
                 match event.kind() {
-                    MIDIEventKind::Channel(channel_event) => {
-                        match channel_event.kind() {
-                            ChannelEventKind::NoteOff {
-                                note,
-                                // TODO: support velocity
-                                velocity: off_velocity,
-                            } => {
-                                let note = MidiNote::new(*note);
-                                if let Some(played_note) =
-                                    played_notes.remove(&(channel_event.channel(), note))
-                                {
-                                    Self::schedule_note(
-                                        ctx,
-                                        destination,
-                                        &periodic_wave,
-                                        note,
-                                        played_note.on_velocity,
-                                        *off_velocity,
-                                        played_note.start_time,
-                                        time - played_note.start_time,
-                                    )?;
-                                }
-                            }
-                            ChannelEventKind::NoteOn { note, velocity } => {
-                                played_notes.insert(
-                                    (channel_event.channel(), MidiNote::new(*note)),
-                                    PlayedNote {
-                                        start_time: time,
-                                        on_velocity: *velocity,
-                                    },
-                                );
-                            }
-                            ChannelEventKind::NoteAftertouch { .. }
-                            | ChannelEventKind::Controller { .. }
-                            | ChannelEventKind::ProgramChange { .. }
-                            | ChannelEventKind::ChannelAftertouch { .. }
-                            | ChannelEventKind::PitchBend { .. } => {
-                                log::warn!("Unhandled channel event: {channel_event:?}")
+                    MIDIEventKind::Channel(channel_event) => match channel_event.kind() {
+                        ChannelEventKind::NoteOff {
+                            note,
+                            velocity: off_velocity,
+                        } => {
+                            let note = MidiNote::new(*note);
+                            if let Some(played_note) =
+                                played_notes.remove(&(channel_event.channel(), note))
+                            {
+                                Self::schedule_note(
+                                    ctx,
+                                    destination,
+                                    &periodic_wave,
+                                    note,
+                                    played_note.on_velocity,
+                                    *off_velocity,
+                                    played_note.start_time,
+                                    time - played_note.start_time,
+                                )?;
                             }
                         }
-                    }
+                        ChannelEventKind::NoteOn { note, velocity } => {
+                            played_notes.insert(
+                                (channel_event.channel(), MidiNote::new(*note)),
+                                PlayedNote {
+                                    start_time: time,
+                                    on_velocity: *velocity,
+                                },
+                            );
+                        }
+                        ChannelEventKind::NoteAftertouch { .. }
+                        | ChannelEventKind::Controller { .. }
+                        | ChannelEventKind::ProgramChange { .. }
+                        | ChannelEventKind::ChannelAftertouch { .. }
+                        | ChannelEventKind::PitchBend { .. } => {
+                            log::warn!("Unhandled channel event: {channel_event:?}")
+                        }
+                    },
                     MIDIEventKind::Meta(MetaEvent::EndOfTrack) => break,
                     MIDIEventKind::Meta(MetaEvent::SetTempo { tempo }) => {
                         tick_duration = self.data.time_division().tick_duration(*tempo);
@@ -118,24 +115,44 @@ impl MidiSynth {
         periodic_wave: &web_sys::PeriodicWave,
         note: MidiNote,
         on_velocity: u8,
-        // TODO: support on and off velocity
-        #[allow(unused_variables)] off_velocity: u8,
+        off_velocity: u8,
         start_time: Duration,
         duration: Duration,
     ) -> Result<(), JsValue> {
         let end_time = start_time + duration;
         let oscillator = web_sys::OscillatorNode::new(ctx)?;
-        let gain = web_sys::GainNode::new(ctx)?;
 
         oscillator.set_periodic_wave(periodic_wave);
         oscillator.frequency().set_value(note.frequency());
         oscillator.start_with_when(start_time.as_secs_f64())?;
         oscillator.stop_with_when(end_time.as_secs_f64())?;
 
+        let gain = web_sys::GainNode::new(ctx)?;
+        // on_velocity used as volume and attack
+        let on_frac = on_velocity as f32 / 127.0;
+        let max_attack_time = Duration::from_millis(100);
+        // harder velocity -> shorter attack
+        let attack_duration =
+            Duration::from_micros((max_attack_time.as_micros() as f32 * (1.0 - on_frac)) as u64)
+                .min(duration / 3);
         gain.gain()
-            .set_value_at_time(on_velocity as f32 / 127.0, start_time.as_secs_f64())?;
+            .set_value_at_time(0.0, start_time.as_secs_f64())?;
+        gain.gain().exponential_ramp_to_value_at_time(
+            on_frac + 0.0001,
+            (start_time + attack_duration).as_secs_f64(),
+        )?;
+
+        // off_velocity used as release
+        let off_frac = off_velocity as f32 / 127.0;
+        let max_release_time = Duration::from_millis(2000);
+        // harder velocity -> shorter release
+        let release_duration =
+            Duration::from_micros((max_release_time.as_micros() as f32 * (1.0 - off_frac)) as u64)
+                .min(duration / 2);
         gain.gain()
-            .linear_ramp_to_value_at_time(0.0001, end_time.as_secs_f64())?;
+            .set_value_at_time(on_frac, (end_time - release_duration).as_secs_f64())?;
+        gain.gain()
+            .exponential_ramp_to_value_at_time(0.0001, end_time.as_secs_f64())?;
 
         oscillator.connect_with_audio_node(&gain)?;
         gain.connect_with_audio_node(destination)?;
